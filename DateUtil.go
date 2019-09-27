c91d458f1669caf4cd750d74e8dda00d51e4d210
sspaas-com <xujianxiong@sspaas.com>
2019-09-27 16:44:11 +0800

    xjx

--- DateUtil.go
+++ DateUtil.go
@@ -1,20 +1,21 @@
 package utils
 
 import (
-	"fmt"
 	"time"
 )
 
 //获取当前时间
 func GetNow() string {
-	now := time.Now()
-	date := now.Format("2006-01-02 15:04:05")
-	fmt.Println(date)
-	return date
+	return time.Now().Format("2006-01-02 15:04:05")
 }
 
 //获取指定时间(年,月,日)前后的时间
 func GetTime(year, month, day int) string {
-	date := time.Now().AddDate(year, month, day).Format("2006-01-02 15:04:05")
-	return date
+	return time.Now().AddDate(year, month, day).Format("2006-01-02 15:04:05")
+
+}
+
+//获取下一个小时的时间
+func GetNextHourTime() string {
+	return time.Now().Add(-time.Hour).Format("2006-01-02 15:04:05")
 }
