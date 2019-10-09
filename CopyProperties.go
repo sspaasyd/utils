8a2a3b7d93f8568c11bf75f5f7875c56b31bd98b
lyh <528510421@qq.com>
2019-10-09 14:23:24 +0800

    lyh

--- CopyProperties.go
+++ CopyProperties.go
@@ -8,10 +8,13 @@ import (
 //源数source据结构可以用值传参，目标target结构体用引用传参,例：&target
 func CopyProperties(source interface{}, target interface{}) {
 	getSourceValue := reflect.ValueOf(source)        //源数据结构
+	getSourceType := reflect.TypeOf(source)          //源数据结构类型
 	getTargetValue := reflect.ValueOf(target).Elem() //目标数据结构
 	for i := 0; i < getTargetValue.NumField(); i++ {
 		targetField := getTargetValue.Type().Field(i)
 		targetFieldValue := getTargetValue.FieldByName(targetField.Name)
-		targetFieldValue.Set(getSourceValue.FieldByName(targetField.Name))
+		if _, ok := getSourceType.FieldByName(targetField.Name); ok {
+			targetFieldValue.Set(getSourceValue.FieldByName(targetField.Name))
+		}
 	}
 }
