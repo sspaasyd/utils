c1b8575c79b56b7a0da4f4170a76d9abf427d381
sspaas-com <xujianxiong@sspaas.com>
2019-10-10 09:44:39 +0800

    xjx

---
+++ DecimalUtil.go
@@ -0,0 +1,18 @@
+package utils
+
+import (
+	"fmt"
+	"strconv"
+)
+
+//小数点位数处理
+
+//先通过Sprintf保留两位小数，再转成float64
+func decimal(f float64, num int) float64 {
+	s1 := "%."
+	s2 := "f"
+	itoa := strconv.Itoa(num)
+	s := s1 + itoa + s2
+	result, _ := strconv.ParseFloat(fmt.Sprintf(s, f), 64)
+	return result
+}
