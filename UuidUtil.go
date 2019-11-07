3c2979af48d3f2cac13abcfab583e03e764d4375
lyh <528510421@qq.com>
2019-11-07 15:51:56 +0800

    lyh

--- UuidUtil.go
+++ UuidUtil.go
@@ -14,11 +14,18 @@ func Get32UUID() string {
 	return id
 }
 
+//获取12位uuid随机数
+func Get12UUID() string {
+	uuids := NewV4()
+	ids := strings.Split(uuids.String(), "-")
+	return ids[0] + ids[1]
+}
+
 const (
-	KC_RAND_KIND_NUM   = 0  // 纯数字
-	KC_RAND_KIND_LOWER = 1  // 小写字母
-	KC_RAND_KIND_UPPER = 2  // 大写字母
-	KC_RAND_KIND_ALL   = 3  // 数字、大小写字母
+	KC_RAND_KIND_NUM   = 0 // 纯数字
+	KC_RAND_KIND_LOWER = 1 // 小写字母
+	KC_RAND_KIND_UPPER = 2 // 大写字母
+	KC_RAND_KIND_ALL   = 3 // 数字、大小写字母
 )
 
 // 随机字符串
@@ -26,23 +33,23 @@ func Krand(size int, kind int) []byte {
 	ikind, kinds, result := kind, [][]int{[]int{10, 48}, []int{26, 97}, []int{26, 65}}, make([]byte, size)
 	is_all := kind > 2 || kind < 0
 	rand.Seed(time.Now().UnixNano())
-	for i :=0; i < size; i++ {
+	for i := 0; i < size; i++ {
 		if is_all { // random ikind
 			ikind = rand.Intn(3)
 		}
 		scope, base := kinds[ikind][0], kinds[ikind][1]
-		result[i] = uint8(base+rand.Intn(scope))
+		result[i] = uint8(base + rand.Intn(scope))
 	}
 	return result
 }
 
 //获取指定长度的随机字符串
-func GetRandomString(i int)string{
-	str:="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-	char:=[]byte(str)
+func GetRandomString(i int) string {
+	str := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	char := []byte(str)
 	var result string
 	rand.Seed(time.Now().UnixNano())
-	for j:=0;j<i;j++{
+	for j := 0; j < i; j++ {
 		intn := rand.Intn(len(str) - 1)
 		s := string(char[intn])
 		result = result + s
